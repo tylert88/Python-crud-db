5cad52256556be0ff3ff365ca5e23daaec47a4ad
Tyler Torres <tyler.john.torres@gmail.com>
2017-12-01 10:08:22 -0700

    New New

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
+
 from automagic_api import app
 # app.run(debug=True, host='0.0.0.0', port=5000)
 port = int(os.environ.get("PORT", 33507))
-app.run(debug=True, port)
+app.run(debug=True, port=port)
 
 
 # postgresql-graceful-65432
