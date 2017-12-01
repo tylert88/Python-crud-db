0c46e8d2d9fae3081e15f90047d4e9f38ef643f4
Tyler Torres <tyler.john.torres@gmail.com>
2017-12-01 10:41:26 -0700

    added host info

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from automagic_api import app
 
 
 port = int(os.environ.get("PORT", 33507))
-app.run(debug=True, port=port)
+app.run(host='0.0.0.0', port=port, debug=True)
 
 # app.run(debug=True, host='0.0.0.0', port=5000)
 # postgresql-graceful-65432
