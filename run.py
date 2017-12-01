9237804d7ff6d75e4cb01cc4e77e410553873691
Tyler Torres <tyler.john.torres@gmail.com>
2017-12-01 10:26:09 -0700

    New New

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os
-
 from automagic_api import app
-# app.run(debug=True, host='0.0.0.0', port=5000)
 
 port = int(os.environ.get("PORT", 33507))
 app.run(debug=True, port=port)
 
-
+# app.run(debug=True, host='0.0.0.0', port=5000)
 # postgresql-graceful-65432
 # https://bright-core.herokuapp.com/api/policy
