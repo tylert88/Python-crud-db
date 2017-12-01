ac69b4ecd53c316cb15b0501c2b31599abbe8ce9
Tyler Torres <tyler.john.torres@gmail.com>
2017-12-01 11:04:47 -0700

    ENV file working

--- run.py
+++ run.py
@@ -3,8 +3,6 @@ from automagic_api import app
 
 
 port = int(os.environ.get("PORT", 33507))
+print("port:")
+print(port)
 app.run(host='0.0.0.0', port=port, debug=True)
-
-# app.run(debug=True, host='0.0.0.0', port=5000)
-# postgresql-graceful-65432
-# https://bright-core.herokuapp.com/api/policy
