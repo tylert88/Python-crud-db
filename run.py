fd4db73fa6b4426e433086fc16cba0fd7cbf4a43
Tyler Torres <tyler.john.torres@gmail.com>
2017-12-01 10:22:55 -0700

    New New

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 
 from automagic_api import app
 # app.run(debug=True, host='0.0.0.0', port=5000)
-if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 33507))
     app.run(debug=True, port=port)
 
